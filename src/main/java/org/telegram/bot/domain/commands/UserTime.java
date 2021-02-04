11a7a36c6f7ec531ee15c9e694f6fb1022593ea4
Dmitry <st-dimka@rambler.ru>
2021-02-04 12:54:26 +0300

    added command /time

---
+++ src/main/java/org/telegram/bot/domain/commands/UserTime.java
@@ -0,0 +1,66 @@
+package org.telegram.bot.domain.commands;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.bot.domain.CommandParent;
+import org.telegram.bot.domain.entities.User;
+import org.telegram.bot.domain.entities.UserCity;
+import org.telegram.bot.services.ChatService;
+import org.telegram.bot.services.UserCityService;
+import org.telegram.bot.services.UserService;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static org.telegram.bot.utils.DateUtils.formatTime;
+
+@Component
+@AllArgsConstructor
+public class UserTime implements CommandParent<SendMessage> {
+
+    private final UserService userService;
+    private final ChatService chatService;
+    private final UserCityService userCityService;
+
+    @Override
+    public SendMessage parse(Update update) {
+        Message message = getMessageFromUpdate(update);
+        String textMessage = cutCommandInText(message.getText());
+        String responseText;
+
+        User user;
+        if (textMessage == null) {
+            Message repliedMessage = message.getReplyToMessage();
+            if (repliedMessage != null) {
+                user = userService.get(repliedMessage.getFrom().getId());
+            } else {
+                user = userService.get(message.getFrom().getId());
+            }
+        } else {
+            user = userService.get(textMessage);
+        }
+
+        if (user == null) {
+            return null;
+        }
+
+        UserCity userCity = userCityService.get(user, chatService.get(message.getChatId()));
+        if (userCity == null) {
+            responseText = "У " + user.getUsername() + " не задан город";
+        } else {
+            String dateTimeNow = formatTime(ZonedDateTime.now(ZoneId.of(userCity.getCity().getTimeZone())));
+            responseText = "У " + user.getUsername() + " сейчас *" + dateTimeNow + "*";
+        }
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.enableMarkdown(true);
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(responseText);
+
+        return sendMessage;
+    }
+}
