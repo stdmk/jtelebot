1215a5dce9cd1ab46cf9d94a1ce66eaeb4ce7846
Dmitry <st-dimka@rambler.ru>
2021-02-04 10:18:01 +0300

    upgraded getid command: someone elses id

--- src/main/java/org/telegram/bot/domain/commands/Getid.java
+++ src/main/java/org/telegram/bot/domain/commands/Getid.java
@@ -3,6 +3,11 @@ package org.telegram.bot.domain.commands;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.bot.domain.CommandParent;
+import org.telegram.bot.domain.entities.User;
+import org.telegram.bot.domain.enums.BotSpeechTag;
+import org.telegram.bot.exception.BotException;
+import org.telegram.bot.services.SpeechService;
+import org.telegram.bot.services.UserService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,11 +16,24 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @AllArgsConstructor
 public class Getid implements CommandParent<SendMessage> {
 
+    private final UserService userService;
+    private final SpeechService speechService;
+
     @Override
-    public SendMessage parse(Update update) {
+    public SendMessage parse(Update update) throws BotException {
         Message message = getMessageFromUpdate(update);
+        String textMessage = getTextMessage(update);
         StringBuilder responseText = new StringBuilder();
         Long chatId = message.getChatId();
+
+        if (textMessage != null) {
+            User user = userService.get(textMessage);
+            if (user == null) {
+                throw new BotException(speechService.getRandomMessageByTag(BotSpeechTag.WRONG_INPUT));
+            }
+
+            responseText.append("Айди ").append(user.getUsername()).append(": `").append(user.getUserId()).append("`\n");
+        }
 
         if (chatId < 0) {
             responseText.append("Айди этого чата: `").append(chatId).append("`\n");
