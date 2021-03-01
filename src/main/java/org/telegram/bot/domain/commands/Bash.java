d069a006d10eeca48f82ea443127308d97bfa8e5
Dmitry <st-dimka@rambler.ru>
2021-03-01 11:28:05 +0300

    refactoring of format text in bash quot

--- src/main/java/org/telegram/bot/domain/commands/Bash.java
+++ src/main/java/org/telegram/bot/domain/commands/Bash.java
@@ -79,8 +79,7 @@ public class Bash implements CommandParent<SendMessage> {
 
         quot = quot.substring(quot.indexOf("color: #21201e\">") + 16);
         quot = quot.substring(0, quot.indexOf("<' + '/div>"));
-        quot = quot.replace("&quot;", "_");
-        quot = quot.replace("<' + 'br>", "\n");
+        quot = formatBashQuot(quot);
 
         return buildResultMessage(quot, quoteNumber, date);
     }
@@ -103,11 +102,18 @@ public class Bash implements CommandParent<SendMessage> {
 
         quot = quot.substring(quot.indexOf("<div class=\"quote__body\">") + 32);
         quot = quot.substring(0, quot.indexOf("</div>"));
+        quot = formatBashQuot(quot);
+
+        return buildResultMessage(quot, quotNumber, date);
+    }
+
+    private String formatBashQuot(String quot) {
         quot = quot.replace("&quot;", "_");
         quot = quot.replace("<br>", "\n");
         quot = quot.replace("<br />", "\n");
+        quot = quot.replace("<' + 'br>", "\n");
 
-        return buildResultMessage(quot, quotNumber, date);
+        return quot;
     }
 
     private String buildResultMessage(String quot, String quotNumber, String date) {
