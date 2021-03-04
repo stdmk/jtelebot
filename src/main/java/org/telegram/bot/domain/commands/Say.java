8558d9dbd8e592bae7478ef6bb6441bfa83012cd
Dmitry <st-dimka@rambler.ru>
2021-03-04 12:13:19 +0300

    added /say and /up commands

--- src/main/java/org/telegram/bot/domain/commands/Say.java
+++ src/main/java/org/telegram/bot/domain/commands/Say.java
@@ -3,25 +3,25 @@ package org.telegram.bot.domain.commands;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.bot.domain.CommandParent;
-import org.telegram.bot.domain.enums.BotSpeechTag;
-import org.telegram.bot.services.SpeechService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 @AllArgsConstructor
-public class Echo implements CommandParent<SendMessage> {
-    private final SpeechService speechService;
-
+public class Say implements CommandParent<SendMessage> {
     @Override
-    public SendMessage parse(Update update) {
+    public SendMessage parse(Update update) throws Exception {
         Message message = getMessageFromUpdate(update);
+        String textMessage = cutCommandInText(message.getText());
+        if (textMessage == null) {
+            return null;
+        }
 
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(message.getMessageId());
-        sendMessage.setText(speechService.getRandomMessageByTag(BotSpeechTag.ECHO));
+        sendMessage.setText(textMessage);
 
         return sendMessage;
     }
