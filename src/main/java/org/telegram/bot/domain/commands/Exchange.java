038d72348016d8685bc512c168001c17d04f1592
Dmitry <st-dimka@rambler.ru>
2022-04-04 16:20:13 +0300

    added currency dynamics emoji

--- src/main/java/org/telegram/bot/domain/commands/Exchange.java
+++ src/main/java/org/telegram/bot/domain/commands/Exchange.java
@@ -9,18 +9,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.bot.domain.CommandParent;
 import org.telegram.bot.domain.enums.BotSpeechTag;
+import org.telegram.bot.domain.enums.Emoji;
 import org.telegram.bot.exception.BotException;
 import org.telegram.bot.services.CommandPropertiesService;
 import org.telegram.bot.services.SpeechService;
+import org.telegram.bot.utils.DateUtils;
 import org.telegram.bot.utils.NetworkUtils;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import javax.annotation.Nullable;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
 
@@ -39,20 +45,23 @@ public class Exchange implements CommandParent<SendMessage> {
         String textMessage = cutCommandInText(message.getText());
         String responseText;
 
-        ValCurs valCurs = getValCursData();
-        List<Valute> valuteList = valCurs.getValute();
-        String cursDate = valCurs.getDate();
+        ValCurs valCursCurrent = getValCursData();
+        String cursDate = valCursCurrent.getDate();
+        ValCurs valCursBefore = getValCursData(LocalDate.parse(cursDate, DateUtils.dateFormatter).minusDays(1));
+
+        List<Valute> valuteList = valCursCurrent.getValute();
+        List<Valute> valuteListYesterday = valCursBefore.getValute();
 
         if (textMessage == null) {
             log.debug("Request to get exchange rates for usd and eur");
-            responseText = getExchangeRatesForUsdAndEur(valuteList, cursDate);
+            responseText = getExchangeRatesForUsdAndEur(valuteList, valuteListYesterday, cursDate);
         } else {
             if (textMessage.startsWith("_")) {
                 textMessage = textMessage.substring(1);
             }
 
             log.debug("Request to get exchange rates for {}", textMessage);
-            responseText = getExchangeRatesForCode(valuteList, textMessage.toUpperCase(Locale.ROOT), cursDate);
+            responseText = getExchangeRatesForCode(valuteList, valuteListYesterday, textMessage.toUpperCase(Locale.ROOT), cursDate);
         }
 
         SendMessage sendMessage = new SendMessage();
@@ -68,15 +77,19 @@ public class Exchange implements CommandParent<SendMessage> {
      * Getting exchange rates for USD and EUR.
      *
      * @param valuteList list with data of exchange rates.
+     * @param date date of current exchange rates.
+     * @param valuteListYesterday exchange rate for yesterday.
      * @return formatted text with exchange rates.
      */
-    private String getExchangeRatesForUsdAndEur(List<Valute> valuteList, String date) {
-        Valute usdValute = getValuteByCode(valuteList, "USD");
-        Valute eurValute = getValuteByCode(valuteList, "EUR");
+    private String getExchangeRatesForUsdAndEur(List<Valute> valuteList, List<Valute> valuteListYesterday, String date) {
+        BigDecimal usdCurrent = getValuteByCode(valuteList, "USD").getValuteValue();
+        BigDecimal usdBefore = getValuteByCode(valuteListYesterday, "USD").getValuteValue();
+        BigDecimal eurCurrent = getValuteByCode(valuteList, "EUR").getValuteValue();
+        BigDecimal eurBefore = getValuteByCode(valuteListYesterday, "EUR").getValuteValue();
 
         return "<b>Курс валют ЦБ РФ:</b>\n" +
-                "$ USD = " + usdValute.getValue() + " RUB\n" +
-                "€ EUR = " + eurValute.getValue() + " RUB\n" +
+                "$ USD = " + usdCurrent + " RUB " + getDynamicEmojiForValuteValue(usdCurrent, usdBefore) + "\n" +
+                "€ EUR = " + eurCurrent + " RUB " + getDynamicEmojiForValuteValue(eurCurrent, eurBefore) + "\n" +
                 "(" + date + ")";
     }
 
@@ -84,18 +97,40 @@ public class Exchange implements CommandParent<SendMessage> {
      * Getting exchange rates for a specific valute.
      *
      * @param valuteList list with data of exchange rates.
+     * @param valuteListYesterday exchange rate for yesterday.
      * @param code code of the valute.
      * @return formatted text with exchange rates.
      */
-    private String getExchangeRatesForCode(List<Valute> valuteList, String code, String date) {
+    private String getExchangeRatesForCode(List<Valute> valuteList, List<Valute> valuteListYesterday, String code, String date) {
         Valute valute = getValuteByCode(valuteList, code);
         if (valute == null) {
             return "Не нашёл валюту <b>" + code + "</b>\nСписок доступных: " + getValuteList(valuteList);
         } else {
+            BigDecimal valuteValueBefore = getValuteByCode(valuteListYesterday, code).getValuteValue();
+
             return "<b>" + valute.getName() + "</b>\n" +
-                    valute.getNominal() + " " + valute.getCharCode() + " = " + valute.getValue() + " RUB\n" +
+                    valute.getNominal() + " " + valute.getCharCode() + " = " + valute.getValue() + " RUB " + getDynamicEmojiForValuteValue(valute.getValuteValue(), valuteValueBefore) + "\n" +
                    "1 RUB = " + Float.parseFloat(valute.getNominal().replaceAll(",", ".")) / Float.parseFloat(valute.getValue().replaceAll(",", ".")) + " " + valute.getCharCode() + "\n" +
                     "(" + date + ")";
+        }
+    }
+
+    /**
+     * Getting currency dynamics emoji.
+     *
+     * @param valuteCurrent current value of Valute.
+     * @param valuteBefore yesterday value of Valute.
+     * @return emoji.
+     */
+    private String getDynamicEmojiForValuteValue(BigDecimal valuteCurrent, BigDecimal valuteBefore) {
+        int compareResult = valuteCurrent.compareTo(valuteBefore);
+
+        if (compareResult > 0) {
+            return Emoji.UP_ARROW.getEmoji();
+        } else if (compareResult < 0) {
+            return Emoji.DOWN_ARROW.getEmoji();
+        } else {
+            return "";
         }
     }
 
@@ -132,12 +167,26 @@ public class Exchange implements CommandParent<SendMessage> {
     }
 
     /**
-     * Getting exchange rates data from service.
+     * Getting current exchange rates data from service.
      *
      * @return list with data of exchange rates.
      */
     private ValCurs getValCursData() {
-        final String xmlUrl = "http://www.cbr.ru/scripts/XML_daily.asp";
+        return getValCursData(null);
+    }
+
+    /**
+     * Getting exchange rates data from service.
+     *
+     * @param date exchange date.
+     * @return list with data of exchange rates.
+     */
+    private ValCurs getValCursData(@Nullable LocalDate date) {
+        String xmlUrl = "http://www.cbr.ru/scripts/XML_daily.asp";
+        if (date != null) {
+            xmlUrl = xmlUrl + "?date_req=" + DateTimeFormatter.ofPattern("dd/MM/yyyy").format(date);
+        }
+
         XmlMapper xmlMapper = new XmlMapper();
         xmlMapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector(TypeFactory.defaultInstance()));
         ValCurs valCurs;
@@ -182,5 +231,9 @@ public class Exchange implements CommandParent<SendMessage> {
 
         @XmlElement(name = "Name")
         private String name;
+
+        public BigDecimal getValuteValue() {
+            return new BigDecimal(this.value.replace(",", "."));
+        }
     }
 }
