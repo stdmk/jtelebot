57a735e802550206b68e3cc8963d33718677e0f6
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-02-08 21:36:02 +0300

    correct #2

--- src/main/java/org/telegram/bot/domain/commands/Uuid.java
+++ src/main/java/org/telegram/bot/domain/commands/Uuid.java
@@ -4,13 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.bot.domain.CommandParent;
-import org.telegram.bot.domain.entities.User;
-import org.telegram.bot.domain.enums.BotSpeechTag;
-import org.telegram.bot.exception.BotException;
-import org.telegram.bot.services.SpeechService;
-import org.telegram.bot.services.UserService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.UUID;
@@ -23,12 +17,11 @@ public class Uuid implements CommandParent<SendMessage> {
 
     @Override
     public SendMessage parse(Update update) {
-        StringBuilder text = new StringBuilder();
-        UUID uuid = UUID.randomUUID();
-        text.append("`").append(uuid).append("`");
+
         SendMessage sendMessage = new SendMessage();
+
         sendMessage.setChatId(update.getMessage().getChatId().toString());
-        sendMessage.setText(text.toString());
+        sendMessage.setText("`" + UUID.randomUUID() + "`");
         sendMessage.setReplyToMessageId(update.getMessage().getMessageId());
         sendMessage.enableMarkdown(true);
 
