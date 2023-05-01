33295198cc7c29b30f4347eac4295a77613c50df
Dmitry <st-dimka@rambler.ru>
2023-05-01 08:41:37 +0300

    added gifs to /truth command

--- src/main/java/org/telegram/bot/domain/commands/Truth.java
+++ src/main/java/org/telegram/bot/domain/commands/Truth.java
@@ -1,9 +1,16 @@
 package org.telegram.bot.domain.commands;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 import org.telegram.bot.domain.CommandParent;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -11,10 +18,12 @@ import static org.telegram.bot.utils.MathUtils.getRandomInRange;
 
 @Component
 @RequiredArgsConstructor
-public class Truth implements CommandParent<SendMessage> {
+public class Truth implements CommandParent<PartialBotApiMethod<?>> {
+
+    private final RestTemplate botRestTemplate;
 
     @Override
-    public SendMessage parse(Update update) {
+    public PartialBotApiMethod<?> parse(Update update) {
         Message message = getMessageFromUpdate(update);
         String textMessage = getTextMessage(update);
         Integer messageIdToReply = message.getMessageId();
@@ -24,17 +33,31 @@ public class Truth implements CommandParent<SendMessage> {
             messageIdToReply = repliedMessage.getMessageId();
         }
 
+        int prob = getRandomInRange(0, 100);
+        String responseText = buildResponseMessage(prob);
+
+        InputFile gif = getGif(prob);
+        if (gif != null) {
+            SendDocument sendDocument = new SendDocument();
+            sendDocument.setChatId(message.getChatId().toString());
+            sendDocument.setCaption(responseText);
+            sendDocument.setParseMode("Markdown");
+            sendDocument.setReplyToMessageId(message.getMessageId());
+            sendDocument.setDocument(gif);
+
+            return sendDocument;
+        }
+
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setReplyToMessageId(messageIdToReply);
         sendMessage.enableMarkdown(true);
-        sendMessage.setText(buildResponseMessage());
+        sendMessage.setText(responseText);
 
         return sendMessage;
     }
 
-    private String buildResponseMessage() {
-        int prob = getRandomInRange(0, 100);
+    private String buildResponseMessage(int prob) {
         String comment;
 
         if (prob == 0) {
@@ -55,4 +78,51 @@ public class Truth implements CommandParent<SendMessage> {
 
         return "Вероятность того, что выражение верно - *" + prob + "%*\n(" + comment + ")";
     }
+
+    private InputFile getGif(int prob) {
+        InputFile inputFile = null;
+
+        Answer answer;
+        if (prob > 50) {
+            answer = Answer.YES;
+        } else if (prob < 50) {
+            answer = Answer.NO;
+        } else {
+            answer = Answer.MAYBE;
+        }
+
+        YesNo yesNo = getYesNo(answer);
+        if (yesNo != null) {
+            inputFile = new InputFile(yesNo.getImage());
+        }
+
+        return inputFile;
+    }
+
+    private YesNo getYesNo(Answer answer) {
+        final String apiUrl = "https://yesno.wtf/api";
+
+        ResponseEntity<YesNo> responseEntity;
+        try {
+            responseEntity = botRestTemplate.getForEntity(apiUrl + "?force=" + answer.name().toLowerCase(), YesNo.class);
+        } catch (RestClientException e) {
+            return null;
+        }
+
+        return responseEntity.getBody();
+    }
+
+    @Data
+    private static class YesNo {
+        private String answer;
+        private Boolean forced;
+        private String image;
+    }
+
+    private enum Answer {
+        YES,
+        NO,
+        MAYBE
+    }
+
 }
