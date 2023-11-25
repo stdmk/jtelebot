1cc64b05ab12b21de179e5cc71e9b2a25ea202db
Dmitry <st-dimka@rambler.ru>
2023-11-25 10:28:00 +0300

    test fix

--- src/test/java/org/telegram/bot/BotApplicationTest.java
+++ src/test/java/org/telegram/bot/BotApplicationTest.java
@@ -4,13 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
+import org.telegram.telegrambots.starter.TelegramBotInitializer;
 
 @SpringBootTest
 public class BotApplicationTest {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @MockBean
+    private TelegramBotInitializer telegramBotInitializer;
 
     @Test
     void runTest() {
