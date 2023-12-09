51b773a563d2eda6bd099749505c816c3b9fee68
Dmitry <st-dimka@rambler.ru>
2023-12-09 09:50:43 +0300

    internationalization of /turn command

--- src/main/java/org/telegram/bot/commands/Turn.java
+++ src/main/java/org/telegram/bot/commands/Turn.java
@@ -3,19 +3,25 @@ package org.telegram.bot.commands;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.telegram.bot.Bot;
 import org.telegram.bot.domain.Command;
 import org.telegram.bot.domain.TextAnalyzer;
 import org.telegram.bot.enums.BotSpeechTag;
 import org.telegram.bot.exception.BotException;
 import org.telegram.bot.services.CommandPropertiesService;
+import org.telegram.bot.services.InternationalizationService;
+import org.telegram.bot.services.LanguageResolver;
 import org.telegram.bot.services.SpeechService;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.telegram.bot.utils.TextUtils.deleteWordsInText;
 
@@ -27,11 +33,23 @@ public class Turn implements Command<SendMessage>, TextAnalyzer {
     private final Bot bot;
     private final SpeechService speechService;
     private final CommandPropertiesService commandPropertiesService;
+    private final LanguageResolver languageResolver;
+    private final InternationalizationService internationalizationService;
 
-    final String RU_LAYOUT = " 1234567890-=йцукенгшщзхъфывапролджэячсмитьбю.\\!\"№;%:?*()_+ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,/";
-    final String EN_LAYOUT = " 1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./\\!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?|";
-    private static final Pattern RU_SYMBOLS_PATTERN = Pattern.compile("[а-яА-Я]+", Pattern.UNICODE_CHARACTER_CLASS);
+    private static final HashSet<Pattern> NOT_NEED_TURN_PATTERNS = new HashSet<>();
+    private static final String EN_LANG_CODE = "en";
+    private static final String EN_LAYOUT = " 1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./\\!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?|";
     private static final Pattern UNTURNED_WORD_SYMPTOM =  Pattern.compile("[qwrtpsdfghjklzxcvbnm]{5}", Pattern.UNICODE_CHARACTER_CLASS);
+
+    @PostConstruct
+    private void postConstruct() {
+        NOT_NEED_TURN_PATTERNS.addAll(
+                internationalizationService.internationalize("${command.turn.pattern}")
+                        .stream()
+                        .filter(StringUtils::hasLength)
+                        .map(Pattern::compile)
+                        .collect(Collectors.toSet()));
+    }
 
     @Override
     public SendMessage parse(Update update) {
@@ -51,7 +69,7 @@ public class Turn implements Command<SendMessage>, TextAnalyzer {
         }
 
         log.debug("Request to turn text: {}", textMessage);
-        String responseText = convert(textMessage);
+        String responseText = convert(textMessage, languageResolver.getChatLanguageCode(update));
 
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(message.getChatId().toString());
@@ -61,13 +79,15 @@ public class Turn implements Command<SendMessage>, TextAnalyzer {
         return sendMessage;
     }
 
-    private String convert(String text) {
+    private String convert(String text, String lang) {
+        String layout = " " + internationalizationService.internationalize("${command.turn.layout}", lang);
+
         if (isEnLayout(text)) {
             log.debug("Request to turn text: {} from EN to RU", text);
-            return convert(EN_LAYOUT, RU_LAYOUT, text);
+            return convert(EN_LAYOUT, layout, text);
         } else {
             log.debug("Request to turn text: {} from RU to EN", text);
-            return convert(RU_LAYOUT, EN_LAYOUT, text);
+            return convert(layout, EN_LAYOUT, text);
         }
     }
 
@@ -108,9 +128,8 @@ public class Turn implements Command<SendMessage>, TextAnalyzer {
         textMessage = deleteWordsInText("@", textMessage);
         textMessage = deleteWordsInText("http", textMessage);
 
-        Matcher matcher = RU_SYMBOLS_PATTERN.matcher(textMessage);
-        if (!matcher.find()) {
-            matcher = UNTURNED_WORD_SYMPTOM.matcher(textMessage);
+        if (!notNeedTurnPatternMatch(textMessage, languageResolver.getChatLanguageCode(update))) {
+            Matcher matcher = UNTURNED_WORD_SYMPTOM.matcher(textMessage);
             if (matcher.find()) {
                 String commandName = commandPropertiesService.getCommand(this.getClass()).getCommandName();
                 Update newUpdate = copyUpdate(update);
@@ -123,4 +142,13 @@ public class Turn implements Command<SendMessage>, TextAnalyzer {
             }
         }
     }
+
+    private boolean notNeedTurnPatternMatch(String text, String lang) {
+        if (EN_LANG_CODE.equals(lang)) {
+            return true;
+        }
+
+        return NOT_NEED_TURN_PATTERNS.stream().map(pattern -> pattern.matcher(text)).map(Matcher::find).findFirst().orElse(false);
+    }
+
 }
