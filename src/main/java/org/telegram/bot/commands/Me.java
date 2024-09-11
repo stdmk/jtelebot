8ecdcd590316c7c3ae51605e57f8b9c38d38ce30
klikli19 <an.kilikova@yandex.ru>
2024-09-11 11:54:39 +0300

    done command me

--- src/main/java/org/telegram/bot/commands/Me.java
+++ src/main/java/org/telegram/bot/commands/Me.java
@@ -6,14 +6,19 @@ import org.springframework.stereotype.Component;
 import org.telegram.bot.domain.model.request.BotRequest;
 import org.telegram.bot.domain.model.request.Message;
 import org.telegram.bot.domain.model.response.BotResponse;
+import org.telegram.bot.domain.model.response.DeleteResponse;
 import org.telegram.bot.domain.model.response.TextResponse;
+import org.telegram.bot.utils.TelegramUtils;
+import org.telegram.bot.utils.TextUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class Me implements Command{
+
     @Override
     public List<BotResponse> parse(BotRequest request) {
         Message message = request.getMessage();
@@ -22,9 +27,14 @@ public class Me implements Command{
             commandArgument.substring(4).trim();
             return returnResponse();
         }
-        String username = request.getMessage().getUser().getUsername();
-        return returnResponse(new TextResponse(request.getMessage())
-                .setText("*@" + username + " " + commandArgument));
+
+        String username = message.getUser().getUsername();
+        return returnResponse(
+                new TextResponse(request.getMessage())
+                .setText("* @" + username + " " + commandArgument),
+                new DeleteResponse(message));
 
     }
+
+
 }
