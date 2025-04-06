f0520a56785e38618ba10a19dc109ea42b9ce2aa
Dmitry <st-dimka@rambler.ru>
2025-04-06 09:14:21 +0300

    refactoring and /convertor tests

--- src/main/java/org/telegram/bot/commands/convertors/MetricUnit.java
+++ src/main/java/org/telegram/bot/commands/convertors/MetricUnit.java
@@ -1,12 +1,10 @@
 package org.telegram.bot.commands.convertors;
 
-import lombok.Getter;
 import lombok.Value;
 
 import java.math.BigDecimal;
 
 @Value
-@Getter
 public class MetricUnit {
     String name;
     BigDecimal multiplier;
